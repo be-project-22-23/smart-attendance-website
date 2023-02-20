b9c63d1025078d5238f7043407963ed9ec76b66d
ashish-sah <ashish.sah@pubmatic.com>
2023-02-20 23:17:56 +0530

    Website Added

---
+++ src/pages/register.tsx
@@ -0,0 +1,287 @@
+import {
+  InboxOutlined,
+  LockOutlined,
+  PhoneOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Upload } from "antd";
+import { Button, Form, Input, Select } from "antd";
+import Link from "next/link";
+import { useState } from "react";
+import { v4 } from "uuid";
+import { storage } from "../utils/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Head from "next/head";
+import axios from "axios";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+const { Option } = Select;
+
+export default function Register() {
+  const [profileImage, setProfileImage] = useState<File>();
+  const router = useRouter();
+
+  const onFinish = (values: any) => {
+    if (profileImage == null) return;
+    const imageRef = ref(storage, `teacher/${profileImage.name + v4()}`);
+    uploadBytes(imageRef, profileImage)
+      .then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((value) => {
+          axios
+            .post(
+              "http://localhost:3000/api/registration/teacher",
+              {
+                email: values.email,
+                name: values.name,
+                department: values.department,
+                phoneNumber: values.phone,
+                photoUrl: value,
+                password: values.password,
+                cpassword: values.confirm,
+              },
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            )
+            .then((value) => {
+              router.push("/");
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const normFile = (e: any) => {
+    const file = e?.fileList[0]?.originFileObj;
+    setProfileImage(file);
+    return e?.fileList;
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Registration - Present Ma'am</title>
+      </Head>
+      <div className="flex flex-col justify-center align-center m-auto max-w-2xl">
+        <Image
+          src="./logo-white.svg"
+          alt="./logo-no-background.svg"
+          width={300}
+          height={300}
+          className="mx-auto"
+        />
+        <Form
+          name="register"
+          onFinish={onFinish}
+          initialValues={{
+            prefix: "91",
+          }}
+          scrollToFirstError
+          className="w-fit mx-auto flex flex-col justify-center"
+        >
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Name"
+            />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+              () => ({
+                validator(_, value) {
+                  if (!value || value.length > 8) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error("Minimum password length is 8!")
+                  );
+                },
+              }),
+            ]}
+            hasFeedback
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item
+            name="confirm"
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error(
+                      "The two passwords that you entered do not match!"
+                    )
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Form.Item>
+          <Form.Item
+            name="department"
+            rules={[
+              {
+                required: true,
+                message: "Please select your department",
+              },
+            ]}
+          >
+            <Select placeholder="Select your department">
+              <Option value="Computer Engineering">Computer Engineering</Option>
+              <Option value="Information Teachnolgy">
+                Information-Teachnolgy
+              </Option>
+              <Option value="Electronics And Telecommunication Engineering">
+                Electronics & Telecommunication Engineering
+              </Option>
+              <Option value="Mechanical Engineering">
+                Mechanical Engineering
+              </Option>
+              <Option value="Electrical Engineering">
+                Electrical Engineering
+              </Option>
+              <Option value="Production Engineering">
+                Production Engineering
+              </Option>
+              <Option value="Chemical Engineering">Chemical Engineering</Option>
+              <Option value="BioTechnology Engineering">
+                BioTechnology Engineering
+              </Option>
+              <Option value="FirstYear Engineering">
+                FirstYear Engineering
+              </Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="phone"
+            rules={[
+              { required: true, message: "Please input your phone number!" },
+              () => ({
+                validator(_, value) {
+                  if (value != null && value.toString().length === 10) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error("Enter corrent phone number")
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input
+              addonBefore={
+                <Form.Item name="prefix" noStyle>
+                  <Select style={{ width: 70 }}>
+                    <Option value="91">+91</Option>
+                  </Select>
+                </Form.Item>
+              }
+              style={{ width: "100%" }}
+              prefix={<PhoneOutlined className="site-form-item-icon" />}
+              placeholder="Phone Number"
+            />
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Please select your profile pic",
+              },
+            ]}
+          >
+            <Form.Item
+              name="dragger"
+              valuePropName="fileList"
+              getValueFromEvent={normFile}
+              noStyle
+            >
+              <Upload.Dragger
+                beforeUpload={() => {
+                  return false;
+                }}
+                name="files"
+              >
+                <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">
+                  Click or drag file to this area to upload
+                </p>
+                <p className="ant-upload-hint">
+                  Please select your profile photo!
+                </p>
+              </Upload.Dragger>
+            </Form.Item>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button bg-[#1677ff] text-white ml-auto"
+            >
+              Register
+            </Button>
+            <div className="mt-3">
+              OR
+              <Link className="ml-2 underline text-[#1677ff]" href="/">
+                Login!
+              </Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/teacher.tsx
@@ -0,0 +1,272 @@
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import { Button, DatePicker, Select, Table } from "antd";
+import { Dayjs } from "dayjs";
+import {
+  defaultClassYear,
+  defaultDepartment,
+  getDownloadUrl,
+} from "../utils/constants";
+import axios from "axios";
+import type { ColumnsType } from "antd/es/table";
+import { useRouter } from "next/router";
+
+const { RangePicker } = DatePicker;
+
+interface DataType {
+  attendanceId: string;
+  classDept: string;
+  classDivision: number;
+  classSubject: string;
+  classYear: string;
+  duration: number;
+  startTime: number;
+  students: string;
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: "Class",
+    dataIndex: "class",
+    key: "class",
+    render: (_, record) => (
+      <p>
+        {record.classYear + "-" + record.classDivision + " " + record.classDept}
+      </p>
+    ),
+    width: "25%",
+  },
+  {
+    title: "Subject",
+    dataIndex: "subject",
+    key: "subject",
+    sorter: (a, b) => a.classSubject.localeCompare(b.classSubject),
+    render: (_, record) => <p>{record.classSubject}</p>,
+  },
+  {
+    title: "Start Date",
+    dataIndex: "startDate",
+    key: "startDate",
+    sorter: (a, b) => a.startTime - b.startTime,
+    render: (_, record) => <p>{new Date(record.startTime).toLocaleString()}</p>,
+  },
+  {
+    title: "End Date",
+    dataIndex: "endDate",
+    key: "endDate",
+    sorter: (a, b) => a.startTime + a.duration - (b.startTime + b.duration),
+    render: (_, record) => (
+      <p>{new Date(record.startTime + record.duration).toLocaleString()}</p>
+    ),
+  },
+  {
+    title: "Attendance Count",
+    dataIndex: "attendanceCount",
+    key: "attendanceCount",
+    sorter: (a, b) =>
+      (a.students !== null ? a.students.split(",").length : 0) -
+      (b.students !== null ? b.students.split(",").length : 0),
+    render: (_, record) => (
+      <p>{record.students !== null ? record.students.split(",").length : 0}</p>
+    ),
+  },
+  {
+    title: "Download Record",
+    key: "downloadRecord",
+    render: (_, record) => (
+      <a
+        href={getDownloadUrl(record.attendanceId)}
+        className="bg-[#1677ff] text-white px-3 rounded-lg py-2"
+      >
+        Download Record
+      </a>
+    ),
+  },
+];
+
+const Teacher = () => {
+  const router = useRouter();
+  const [teacherId, setTeacherId] = useState<String>();
+  const [classYear, setClassYear] = useState(defaultClassYear);
+  const [department, setDepartment] = useState(defaultDepartment);
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [attendances, setAttendances] = useState([]);
+  const [displayAttendance, setDisplayAttendance] = useState(attendances);
+  const [teacherDetails, setTeacherDetails] = useState<any>();
+  const handleClassYearChange = (value: string[]) => {
+    setClassYear(
+      defaultClassYear?.filter(
+        (option) =>
+          value.find((element) => {
+            return element === option.label;
+          }) != null
+      )
+    );
+  };
+
+  const handleDepartmentChange = (value: string[]) => {
+    setDepartment(
+      defaultDepartment?.filter(
+        (option) =>
+          value.find((element) => {
+            return element === option.label;
+          }) != null
+      )
+    );
+  };
+
+  const onRangeChange = (dates: null | (Dayjs | null)[]) => {
+    if (dates && dates.length === 2) {
+      setStartTime(new Date(dates[0]!!.toDate()!!).getTime());
+      setEndTime(new Date(dates[1]!!.toDate()!!).getTime() + 86399999);
+    }
+  };
+
+  useEffect(() => {
+    const id = localStorage.getItem("id");
+    if (id != null) {
+      setTeacherId(id);
+      axios
+        .get(`http://localhost:3000/api/teacherDetails?teacherId=${id}`)
+        .then((data) => {
+          setTeacherDetails(data.data.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (teacherId === null || !teacherId) return;
+    axios
+      .get(`http://localhost:3000/api/getAllAttendance?teacherId=${teacherId}`)
+      .then((data) => {
+        const newData = data.data.data.map((attendance: any) => {
+          return {
+            attendanceId: attendance.attendanceId,
+            class:
+              attendance.classYear +
+              "-" +
+              attendance.classDivision +
+              " " +
+              attendance.classDept,
+            subject: attendance.classSubject,
+            startTime: new Date(attendance.startTime).toLocaleTimeString(),
+            attendanceCount:
+              attendance.students !== null
+                ? attendance.students.split(",").length
+                : 0,
+          };
+        });
+        setAttendances(data.data.data);
+        setDisplayAttendance(data.data.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [teacherId]);
+
+  useEffect(() => {
+    const newDisplayAttendance = attendances.filter((attendance: any) => {
+      return classYear?.find((ele) => {
+        return ele.label === attendance.classYear;
+      });
+    });
+    setDisplayAttendance(newDisplayAttendance);
+  }, [classYear]);
+  useEffect(() => {
+    const newDisplayAttendance = attendances.filter((attendance: any) => {
+      return department?.find((ele) => {
+        return ele.label === attendance.classDept;
+      });
+    });
+    setDisplayAttendance(newDisplayAttendance);
+  }, [department]);
+  useEffect(() => {
+    const newDisplayAttendance = attendances.filter((attendance: any) => {
+      return (
+        attendance.startTime >= startTime &&
+        attendance.startTime + attendance.duration <= endTime
+      );
+    });
+    setDisplayAttendance(newDisplayAttendance);
+  }, [startTime, endTime]);
+
+  return (
+    <div className="overflow-x-hidden">
+      <Head>
+        <title>
+          {teacherDetails !== null ? teacherDetails?.name : "Teacher"} - Present
+          Ma'am
+        </title>
+      </Head>
+      <div className="flex items-center mt-5 justify-between px-5">
+        <p className="font-bold text-center flex-1">
+          Hey, {teacherDetails?.name}! Welcome to Present Ma'am
+        </p>
+        <Button
+          onClick={() => {
+            localStorage.removeItem("email");
+            localStorage.removeItem("password");
+            localStorage.removeItem("id");
+            router.push("/");
+          }}
+          type="primary"
+          className="text-white bg-[#1677ff] ml-2"
+        >
+          Logout {teacherDetails?.name}
+        </Button>
+      </div>
+      <nav className="p-5">
+        <Select
+          mode="multiple"
+          allowClear
+          placeholder="Please select the class"
+          defaultValue={["FE", "SE", "TE", "BE"]}
+          onChange={handleClassYearChange}
+          options={defaultClassYear}
+        />
+        <Select
+          mode="multiple"
+          allowClear
+          style={{ width: "50%" }}
+          maxTagCount="responsive"
+          className="ml-2"
+          placeholder="Please select the departent"
+          defaultValue={[
+            "Computer Engineering",
+            "Information-Teachnolgy",
+            "Electronics & Telecommunication Engineering",
+            "Mechanical Engineering",
+            "Electrical Engineering",
+            "Chemical Engineering",
+            "Production Engineering",
+            "BioTechnology Engineering",
+            "FirstYear Engineering",
+          ]}
+          onChange={handleDepartmentChange}
+          options={defaultDepartment}
+        />
+        <RangePicker className="ml-2" onChange={onRangeChange} />
+        <Button
+          type="primary"
+          onClick={() => router.push("/create")}
+          className="text-white bg-[#1677ff] ml-2"
+        >
+          Create Attendance
+        </Button>
+      </nav>
+      <main className="p-5">
+        <Table
+          pagination={false}
+          columns={columns}
+          dataSource={displayAttendance}
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Teacher;
