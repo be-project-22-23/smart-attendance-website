2b664e99d604175511cc47e500e9bb3f8bbf1317
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2023-03-25 15:20:16 +0530

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -21,10 +21,10 @@ export const defaultClassYear: SelectProps["options"] = [
 
 export const defaultDepartment: SelectProps["options"] = [
   { label: "Computer Engineering", value: "Computer Engineering" },
-  { label: "Information-Teachnolgy", value: "Information-Teachnolgy" },
+  { label: "Information Technology", value: "Information Technology" },
   {
-    label: "Electronics & Telecommunication Engineering",
-    value: "Electronics & Telecommunication Engineering",
+    label: "Electronics And Telecommunication Engineering",
+    value: "Electronics And Telecommunication Engineering",
   },
   { label: "Mechanical Engineering", value: "Mechanical Engineering" },
   { label: "Electrical Engineering", value: "Electrical Engineering" },
@@ -32,6 +32,8 @@ export const defaultDepartment: SelectProps["options"] = [
   { label: "Production Engineering", value: "Production Engineering" },
   { label: "BioTechnology Engineering", value: "BioTechnology Engineering" },
   { label: "FirstYear Engineering", value: "FirstYear Engineering" },
+  { label: "Civil Engineering", value: "Civil Engineering" },
+  { label: "Instrumental Engineering", value: "Instrumental Engineering" },
 ];
 
 export function getDownloadUrl(attendanceId: any): string | undefined {
